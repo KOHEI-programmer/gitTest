10521c6e37113904361dfc1d47254737d29b8743
KOHEI-programmer <kw20040623@gmail.com>
2024-10-22 17:43:08 +0900

    hファイル作成

--- main.cpp
+++ main.cpp
@@ -1,43 +1,43 @@
+#include"question.h"
 #include<iostream>
 #include<string>
 #include<random>
 using namespace std;
 
-//	問題文と答えをまとめる構造体
-struct Question
-{
-	string q;	//	問題文
-	int a;		//	答え
-};
-
 int main()
 {
-	Question questions[3];
+	Question questions[4];
 
 	random_device rd;
 	mt19937 rand(rd());
 
 	//	掛け算
 
-	int x = uniform_int_distribution<>(1, 30)(rand);
-	int y = uniform_int_distribution<>(1, 20)(rand);
+	int x = uniform_int_distribution<>(1, 300)(rand);
+	int y = uniform_int_distribution<>(1, 200)(rand);
 	questions[0].q = to_string(x) + "x" + to_string(y);
 	questions[0].a = x * y;
 
 	//	割り算
-	x = uniform_int_distribution<>(1, 30)(rand);
-	y = uniform_int_distribution<>(1, 20)(rand);
+	x = uniform_int_distribution<>(1, 300)(rand);
+	y = uniform_int_distribution<>(1, 200)(rand);
 	questions[1].q = to_string(x * y) + "÷" + to_string(y);
 	questions[1].a = x;
 
 	//	複雑な式
-	x = uniform_int_distribution<>(1, 100)(rand);
-	y = uniform_int_distribution<>(1, 10)(rand);
-	int z = uniform_int_distribution<>(1, 10)(rand);
-	int w = uniform_int_distribution<>(1, 10)(rand);
+	x = uniform_int_distribution<>(1, 1000)(rand);
+	y = uniform_int_distribution<>(1, 100)(rand);
+	int z = uniform_int_distribution<>(1, 100)(rand);
+	int w = uniform_int_distribution<>(1, 100)(rand);
 	questions[2].q =
 		to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w);
 	questions[2].a = x - (y + z);
+
+	//	累乗
+	x = uniform_int_distribution<>(2, 50)(rand);
+	y = uniform_int_distribution<>(2, 5)(rand);
+	questions[3].q = to_string(x) + "^" + to_string(y);
+	questions[3].a = static_cast<int>(pow(x, y));
 
 	cout << "[リクルート試験対策クイズ]\n";
 
